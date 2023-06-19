b35a0b4e83fcd11b8c8bc22286d6e2afb39c8add
Mohit Rajan E <mohit.rajan@bigbinary.com>
2023-06-19 18:38:19 +0530

    Add ability to delete a contact

---
+++ app/javascript/src/components/Dashboard/Contacts/DeleteAlert.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+import { Alert, Toastr } from "neetoui";
+import { useTranslation } from "react-i18next";
+
+const DeleteAlert = ({
+  onClose,
+  selectedContactId,
+  setSelectedContactId,
+  setContacts,
+  contacts,
+}) => {
+  const [deleting, setDeleting] = useState(false);
+
+  const { t } = useTranslation();
+
+  const handleDelete = () => {
+    setDeleting(true);
+    setContacts(contacts.filter(note => note.id !== selectedContactId));
+    onClose();
+    Toastr.success(t("contacts.deleteSuccess"));
+    setSelectedContactId("");
+    setDeleting(false);
+  };
+
+  return (
+    <Alert
+      isOpen
+      isSubmitting={deleting}
+      message={t("contacts.deleteDescription")}
+      title={t("contacts.deleteTitle")}
+      onClose={onClose}
+      onSubmit={handleDelete}
+    />
+  );
+};
+
+export default DeleteAlert;
